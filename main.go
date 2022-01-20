ed078ca222ef6571342104ae6afeab6375378793
Jonas Knobloch <jonas.knobloch@t-online.de>
2022-01-20 16:35:27 +0100

    Use appropriate config field for training sample limit

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/jonasknobloch/jinn/pkg/tree"
-)
+import "github.com/jonasknobloch/jinn/pkg/tree"
 
 func main() {
 	tr := &tree.Tree{
@@ -31,8 +29,5 @@ func main() {
 
 	pCache["α β"] = NewMetaTree(tr)
 
-	tCache["α β"] = []string{"α", "β"}
-	tCache["s b a"] = []string{"s", "b", "a"}
-
-	TrainEM(Config.TrainingIterationLimit, Config.TrainingIterationLimit)
+	TrainEM(Config.TrainingIterationLimit, Config.TrainingSampleLimit)
 }
