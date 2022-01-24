bd981545b562e555aee35bae74fb610b5429b56b
Jonas Knobloch <jonas.knobloch@t-online.de>
2022-01-24 12:28:34 +0100

    Add optional CPU profiling

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/jonasknobloch/jinn/pkg/tree"
+import (
+	"flag"
+	"github.com/jonasknobloch/jinn/pkg/tree"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuProfile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
+	flag.Parse()
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal(err)
+		}
+
+		defer pprof.StopCPUProfile()
+	}
+
 	tr := &tree.Tree{
 		Label: "σ",
 		Children: []*tree.Tree{
